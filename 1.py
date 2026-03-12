1d05a2ace74565749fc400eac785f388fa6daaf5
saisaharshnaidu29-cyber <saisaharshnaidu.29@gmail.com>
2026-03-12 16:44:19 +0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the have boolean value as true")
+else:
+    print("At least one of them have boolean value as false")
+
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than zero")
+else:
+    print("no number is greater than zero")
+
+if b > 0 or c > 0:
+        print("Either of the number is greater than zero")
+else:
+        print("no number is greater than zero")
